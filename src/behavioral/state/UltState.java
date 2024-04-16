56dff053bb563ed0272db7497040204204cf6867
Viktoriia Kozminska <kozminska.viktoriia@chnu.edu.ua>
2024-04-16 11:05:48 +0300

    State is created

--- src/behavioral/state/UltState.java
+++ src/behavioral/state/UltState.java
@@ -4,6 +4,5 @@ public class UltState implements PlayerState {
     @Override
     public void handle() {
         System.out.println("Player is in ult state.");
-        // Додаткові дії для ульті стану
     }
 }
